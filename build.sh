e4ebf0111702e57bf668e2ec79ea0ead153c711d
Emir D <emirde@stud.ntnu.no>
2023-08-02 14:44:19 +0200

    add build script

---
+++ build.sh
@@ -0,0 +1,6 @@
+bazel build //validator:image_bundle --config=release
+bazel build //beacon-chain:image_bundle --config=release
+bazel build cmd/validator:image_bundle.tar
+bazel build cmd/beacon-chain:image_bundle.tar
+docker load -i bazel-bin/cmd/validator/image_bundle.tar
+docker load -i bazel-bin/cmd/beacon-chain/image_bundle.tar
